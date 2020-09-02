d817bbfcc06db20b6228eb42607060dd0b0a3a31
Michal <0kaczmar9@gmail.com>
2020-09-02 21:32:16 +0200

    Start

---
+++ src/main/java/pl/kaczmar/AgileProject/AgileProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.kaczmar.AgileProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kaczmar/AgileProject/AgileProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kaczmar.AgileProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
